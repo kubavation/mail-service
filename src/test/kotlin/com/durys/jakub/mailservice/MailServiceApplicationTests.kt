0d848fd7863f0f705bc70a444f431e2eca084c70
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-14 18:52:00 +0100

    init

---
+++ src/test/kotlin/com/durys/jakub/mailservice/MailServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.durys.jakub.mailservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
