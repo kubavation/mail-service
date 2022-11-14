0e55f05b445c316d51b46bd2caf514cb7989749f
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-14 20:24:40 +0100

    basic mail configuration

--- src/main/kotlin/com/durys/jakub/mailservice/configuration/MailConfiguration.kt
+++ src/main/kotlin/com/durys/jakub/mailservice/configuration/MailConfiguration.kt
@@ -1,4 +1,32 @@
 package com.durys.jakub.mailservice.configuration
 
-class MailConfiguration {
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.JavaMailSenderImpl
+
+@Configuration
+class MailConfiguration(@Value("\${mail-host}") private val host: String,
+                        @Value("\${mail-port}") private val port: Int,
+                        @Value("\${mail-username}") private val username: String,
+                        @Value("\${mail-password}") private val password: String) {
+
+
+
+    @Bean
+    fun mailSender(): JavaMailSender {
+        val mailSender = JavaMailSenderImpl()
+        mailSender.host = host
+        mailSender.port = port
+        mailSender.username = username
+        mailSender.password = password
+
+        mailSender.javaMailProperties.setProperty("mail.transport.protocol", "smtp")
+        mailSender.javaMailProperties.setProperty("mail.smtp.auth", "true")
+        mailSender.javaMailProperties.setProperty("mail.smtp.starttls.enable", "true")
+        mailSender.javaMailProperties.setProperty("mail.debug", "true")
+        return mailSender
+    }
+
 }
