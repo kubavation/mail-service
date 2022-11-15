3c2325c00af4d4612d79155028c705ecd7a800ef
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-15 20:17:11 +0100

    eureka client config

--- src/main/kotlin/com/durys/jakub/mailservice/mail/converter/BaseMailConverter.kt
+++ src/main/kotlin/com/durys/jakub/mailservice/mail/converter/BaseMailConverter.kt
@@ -1,7 +1,6 @@
 package com.durys.jakub.mailservice.mail.converter
 
 import com.durys.jakub.mailservice.mail.model.BaseMailDTO
-import org.springframework.mail.MailMessage
 import org.springframework.mail.SimpleMailMessage
 
 class BaseMailConverter: MailConverter<BaseMailDTO, SimpleMailMessage> {
