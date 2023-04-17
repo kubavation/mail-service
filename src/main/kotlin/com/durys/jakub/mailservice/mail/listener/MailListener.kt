20dd5fdb54d3b9846ace1fcb3f5b018a51311550
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-17 18:39:31 +0200

    fix

--- src/main/kotlin/com/durys/jakub/mailservice/mail/listener/MailListener.kt
+++ src/main/kotlin/com/durys/jakub/mailservice/mail/listener/MailListener.kt
@@ -3,5 +3,5 @@ package com.durys.jakub.mailservice.mail.listener
 import com.durys.jakub.mailservice.mail.model.BaseMailDTO
 
 internal interface MailListener {
-    fun send(mail: BaseMailDTO);
+    fun send(mail: BaseMailDTO)
 }
