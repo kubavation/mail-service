c3bdae5f30ab4cf812b65cb27744b531d334d440
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-17 18:25:10 +0200

    listener method in rabbitmqMailListener

--- src/main/kotlin/com/durys/jakub/mailservice/mail/listener/RabbitmqMailListener.kt
+++ src/main/kotlin/com/durys/jakub/mailservice/mail/listener/RabbitmqMailListener.kt
@@ -1,14 +1,13 @@
 package com.durys.jakub.mailservice.mail.listener
 
 import com.durys.jakub.mailservice.mail.model.BaseMailDTO
+import com.durys.jakub.mailservice.mail.service.MailService
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Service
 
 @Service
-class RabbitmqMailListener: MailListener {
+internal class RabbitmqMailListener(private val mailService: MailService): MailListener {
 
     @RabbitListener(queues = ["\${q.mail-queue}"])
-    override fun send(mail: BaseMailDTO) {
-        //todo
-    }
+    override fun send(mail: BaseMailDTO) =mailService.convertAndSend(mail)
 }
