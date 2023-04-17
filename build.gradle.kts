12edebd9cd72b6f3b88309293ab6e0357afa8265
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-17 18:04:11 +0200

    amqp dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.cloud:spring-cloud-config-client")
 }
 
