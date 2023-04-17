71913da2da1905181c9154c455b244fb6eac7772
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-17 18:36:18 +0200

    jackson message converter config

--- src/main/kotlin/com/durys/jakub/mailservice/mail/listener/QueueConfiguration.kt
+++ src/main/kotlin/com/durys/jakub/mailservice/mail/listener/QueueConfiguration.kt
@@ -1,6 +1,7 @@
 package com.durys.jakub.mailservice.mail.listener
 
 import org.springframework.amqp.core.Queue
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,4 +11,7 @@ class QueueConfiguration {
 
     @Bean
     fun notificationQueue(@Value("\${q.mail-queue}") mailQueue: String) = Queue(mailQueue)
+
+    @Bean
+    fun converter() = Jackson2JsonMessageConverter()
 }
