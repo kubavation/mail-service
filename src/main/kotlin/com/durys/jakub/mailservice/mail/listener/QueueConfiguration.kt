41b3f82d9ee4c1e41a02045755433419c9acb538
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-17 18:35:28 +0200

    mail queue config

---
+++ src/main/kotlin/com/durys/jakub/mailservice/mail/listener/QueueConfiguration.kt
@@ -0,0 +1,13 @@
+package com.durys.jakub.mailservice.mail.listener
+
+import org.springframework.amqp.core.Queue
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class QueueConfiguration {
+
+    @Bean
+    fun notificationQueue(@Value("\${q.mail-queue}") mailQueue: String) = Queue(mailQueue)
+}
